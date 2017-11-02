2105cdd7792fbcc1779ceb9e909a60ca7429d40e
armoner24 <aadityavaidya97@gmail.com>
2017-11-02 18:10:18 +0530

    change

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import tornado.ioloop
 import tornado.web
 import os
 import time
-import datetime
 from hackerrank import hackerrank
 from codeforces import codeforces
-from codechef import codechef
-
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
 
@@ -16,36 +13,21 @@ class UpcomingHandler(tornado.web.RequestHandler):
     def get(self):
         hackerrank_contests=hackerrank()
         codeforces_contests=codeforces()
-        codechef_contests=codechef()
-        self.render("upcoming.html",hackerrank_contests=hackerrank_contests,codeforces_contests=codeforces_contests,codechef_contests=codechef_contests)
+        self.render("upcoming.html",hackerrank_contests=hackerrank_contests,codeforces_contests=codeforces_contests)
 
-class OngoingHandler(tornado.web.RequestHandler):
-    def get(self):
-        hackerrank_contests=hackerrank()
-        codeforces_contests=codeforces()
-        
-        cur=datetime.datetime.now()
-        ongoing_hackerrank=[]
-        ongoing_codeforces=[]
-        for contest in hackerrank_contests:
-            if datetime.datetime.strptime(contest[1],"%Y-%m-%d %H:%M:%S")<=cur<=datetime.datetime.strptime(contest[2],"%Y-%m-%d %H:%M:%S"):
-                ongoing_hackerrank.append(contest)
-        for contest in codeforces_contests:
-            if datetime.datetime.strptime(contest[1],"%Y-%m-%d %H:%M:%S")<=cur<=datetime.datetime.strptime(contest[2],"%Y-%m-%d %H:%M:%S"):
-                ongoing_codeforces.append(contest)
-        self.render("ongoing.html",hackerrank_contests=ongoing_hackerrank,codeforces_contests=ongoing_codeforces)
+ 
+
 if __name__ == "__main__":
     application = tornado.web.Application([
         (r"/", MainHandler),
         (r"/upcoming", UpcomingHandler),
-        (r"/ongoing", OngoingHandler),
         (r"/images/(.*)", tornado.web.StaticFileHandler, {'path': "./images"}),
         (r"/css/(.*)", tornado.web.StaticFileHandler, {'path': "./css"}),
         (r"/fonts/(.*)", tornado.web.StaticFileHandler, {'path': "./fonts"})
     
     ])
     http_server = tornado.httpserver.HTTPServer(application)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     http_server.listen(port)
     tornado.ioloop.IOLoop.instance().start()
     
